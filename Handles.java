9a805fd240dafd82d5991f8a26f672d279482290
Felip7 <esteban.diaz@epn.edu.ec>
2015-12-17 09:05:47 -0500

    Create Handles.java

---
+++ Handles.java
@@ -0,0 +1,54 @@
+
+package Sintax;
+ import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.StringTokenizer;
+ 
+public class Handles {
+   
+ 
+        public static void main(String[] args) {
+               
+                try
+                {
+                       
+                        //csv file containing data
+                        String strFile = "D:\\k.csv";
+                       
+                        //create BufferedReader to read csv file
+                        BufferedReader br = new BufferedReader( new FileReader(strFile));
+                        String strLine = "";
+                        StringTokenizer st = null;
+                        int lineNumber = 0, tokenNumber = 0;
+                       
+                        //read comma separated file line by line
+                        while( (strLine = br.readLine()) != null)
+                        {
+                                lineNumber++;
+                               
+                                //break comma separated line using ","
+                                st = new StringTokenizer(strLine, ",");
+                               
+                                while(st.hasMoreTokens())
+                                {
+                                        //display csv values
+                                        tokenNumber++;
+                                        System.out.println("Line # " + lineNumber +
+                                                        ", Token # " + tokenNumber
+                                                        + ", Token : "+ st.nextToken());
+                                }
+                               
+                                //reset token number
+                                tokenNumber = 0;
+                               
+                        }
+                       
+                       
+                }
+                catch(Exception e)
+                {
+                        System.out.println("Exception while reading csv file: " + e);                  
+                }
+        }
+}
+
