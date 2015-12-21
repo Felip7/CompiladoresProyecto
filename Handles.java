8f5f3997d65284f9ce8c754c431ddeafeeca73fe
Felip7 <esteban.diaz@epn.edu.ec>
2015-12-21 08:35:47 -0500

    Update Handles.java

--- Handles.java
+++ Handles.java
@@ -13,12 +13,12 @@ public class Handles {
                 {
                        
                         //csv file containing data
-                        String strFile = "D:\\salida.csv";
+                        String strFile = "D:\\gramatica.txt";
                        
                         //create BufferedReader to read csv file
                         BufferedReader br = new BufferedReader( new FileReader(strFile));
-                        String strLine = "";
-                        StringTokenizer st = null;
+                        String strLine ;
+                        StringTokenizer st ;
                         int lineNumber = 0, tokenNumber = 0;
                        
                         //read comma separated file line by line
@@ -31,19 +31,32 @@ public class Handles {
                                
                                 while(st.hasMoreTokens())
                                 {
-                                        //display csv values
-                                        tokenNumber++;
-                                        System.out.println("Line # " + lineNumber +
-                                                        ", Token # " + tokenNumber
-                                                        + ", Token : "+ st.nextToken());
-                                }
-                               
-                                //reset token number
-                                tokenNumber = 0;
-                               
-                        }
-                       
-                       
+                                         if (strLine.contains(" "))
+                                         {  
+                                             String [] palabras = strLine.split(" ");
+                                             tokenNumber++;
+                                             
+                                             for(int z=0; z<=tokenNumber; z++)           
+                                             {
+                                                 String part1 = palabras[z];
+                                                 String part2 = palabras[z+1];
+//                                                 String part3 = palabras[z+2];
+//                                                 String part4 = palabras[z+3];
+//                                                 System.out.println(part1+"__"+part2+"__"+part3+"__"+part4);
+                                                 Sintactico sx = new Sintactico(part1, part2);
+                                                 break; 
+                                             }
+                                         }
+                                             else
+                                             {
+//                                             System.out.println("Line # " + lineNumber +
+//                                                     ", Token # " + tokenNumber
+//                                                     + ", Token : "+ st.nextToken());
+//                                             break;
+                                              }
+                                         break;
+                                        }
+                        } tokenNumber = 0;
                 }
                 catch(Exception e)
                 {
