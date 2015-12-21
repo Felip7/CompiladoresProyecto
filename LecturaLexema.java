f102cecfaaf88515d310da910f47965385cd74eb
Felip7 <esteban.diaz@epn.edu.ec>
2015-12-20 23:01:36 -0500

    Update LecturaLexema.java

--- LecturaLexema.java
+++ LecturaLexema.java
@@ -2,6 +2,7 @@ package App;
 
 import static App.TokenClass.Especial;
 import static App.TokenClass.Letra;
+import static App.TokenClass.Numeros;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.DataInputStream;
@@ -26,121 +27,31 @@ public class LecturaLexema {
                     {//recorremos la línea
                         
                         char t=cad.charAt(i);//vamos leyendo caracter por caracter
-                        int j=i+1;
-                      
                         if(Character.isDigit(t))
                         {//comprobamos si es un digito
-                            String ca="";
-                            ca+=t;
-                          
-                           if (cad.length()==1)
-                                   {
-                                       System.out.println("Número, "+t);
-                                    fw1.write("Numero, "+t);
-                                    fw1.newLine();
-                                    fw1.flush(); 
-                                    break;
-                                   }
-                           
-                          
-                                                               
-                              if(cad.length()==2)
-                              {
-                                   if(Character.isDigit(cad.charAt(j)))
-                                   {
-                                       while(j!=cad.length())
-                                       {
-                                           // mientras el siguiente elemento sea un numero
-                                           
-                                           ca+=cad.charAt(j);//concatenamos
-                                           j++;
-                                           System.out.println("Número, "+ca);
-                                           fw1.write("Numero, "+ca);
-                                           fw1.newLine();
-                                           fw1.flush();
-                                        }
-                                     i=j;              
-                                    }
-                                   else if(Character.isLetter(cad.charAt(1)))
-                                  {
-                                         System.out.println(cad+" ERROR "+"en línea " +NumeroLineas);
-                                          break;
-                                  }
-                                   break;
-                              }
-                               
-                             
-                                 
-                            if(cad.length()==3)
-                              {
-                                  if(Character.isDigit(cad.charAt(2)))
-                                   {
-                                      
-                                           // mientras el siguiente elemento sea un numero
-                                           String sd="";
-                                           sd+=cad.charAt(0);//concatenamos
-                                           sd+=cad.charAt(1);
-                                           sd+=cad.charAt(2);
-                                           
-                                          
-                                           System.out.println("Número, "+sd);
-                                           fw1.write("Numero, "+sd);
-                                           fw1.newLine();
-                                           fw1.flush();
-                                         j++;
-                                     i=j;             
-                                   }
-                                  
-                                  else if(Character.isLetter(cad.charAt(2))==true)
-                                  {
-                                         System.out.println(cad+" ERROR "+"en línea " +NumeroLineas);
-                                          break;
-                                  }
-                               break;
-                              
-                              }
-                               
-                                if(cad.length()==4)
-                              { 
-                                   if(Character.isDigit(cad.charAt(j)) && j!=cad.length())
-                                   {
-//                                       while(j!=cad.length())
-//                                       {
-//                                            mientras el siguiente elemento sea un numero
-                                           String sd="";
-                                           sd+=cad.charAt(1);//concatenamos
-                                           sd+=cad.charAt(2);
-                                           sd+=cad.charAt(3);
-                                           System.out.println("Número, "+t+sd);
-                                           fw1.write("Numero, "+ca+sd);
-                                           fw1.newLine();
-                                           fw1.flush();
-                                           j++;
-                                        }
-                                     i=j;              
-//                                    }
-                               break;
-                                     
-                           }   
+                            Numeros(t, cad, fw1, NumeroLineas);
+                            break;
                         }
 //                        end if si es Digito    
                         else if(Character.isLetter(t))
                         {//comprobamos si es una letra
                             
-                            Letra(t,i,j,cad, fw1);
+                            Letra(t,cad, fw1, NumeroLineas );
                             break;
                         }
                              
 //                              mientras el siguiente elemento no sea una letra o un digito
                         else  if (Character.isLetterOrDigit(cad.charAt(0))==false ) {
                            
-                            Especial(t,cad,fw1,NumeroLineas);
+                            Especial(t,cad.charAt(i), cad,fw1,NumeroLineas);
                             break;
                         }
+                        i++;
                     }
                     
                     NumeroLineas++;
-        }
+                    }
+        
         
         } catch (IOException ex) {
             Logger.getLogger(LecturaLexema.class.getName()).log(Level.SEVERE, null, ex);
