d5624940a7aebe3e2e0b18808a0f2909728c2ecf
Felip7 <esteban.diaz@epn.edu.ec>
2016-01-29 11:06:30 -0500

    Create Etapas.java

---
+++ Etapas.java
@@ -0,0 +1,25 @@
+package Control;
+
+import App.CrearTexto;
+import Syntax.Sintactico;
+import java.io.IOException;
+
+public class Etapas {
+    
+      public static void main(String args[]) throws IOException {
+        
+          //Analizador Léxico
+//        CrearTexto ct=new CrearTexto();
+//        ct.creaArchivoTexto("D:\\", "salida");
+
+        //Analizador Sintáctico
+//          LeerCSV csv=new LeerCSV();
+//          LeerCSV.muestraContenido("D:\\salida.csv");
+//          
+            Sintactico sx = new Sintactico();
+//           
+        
+    
+    
+      }
+}
