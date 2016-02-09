2983b4a17f507b7719b1d419241117d15cebe36a
Felip7 <esteban.diaz@epn.edu.ec>
2016-02-09 16:24:56 -0500

    Update Etapas.java

--- Etapas.java
+++ Etapas.java
@@ -1,25 +1,33 @@
 package Control;
 
-import App.CrearTexto;
+import Lex.CrearTexto;
 import Syntax.Sintactico;
 import java.io.IOException;
 
 public class Etapas {
     
+    public static int lineas;
+    public static int lineas1;
+    
       public static void main(String args[]) throws IOException {
         
-          //Analizador Léxico
-//        CrearTexto ct=new CrearTexto();
-//        ct.creaArchivoTexto("D:\\", "salida");
-
-        //Analizador Sintáctico
-//          LeerCSV csv=new LeerCSV();
-//          LeerCSV.muestraContenido("D:\\salida.csv");
-//          
-            Sintactico sx = new Sintactico();
-//           
         
+//          Analizador Léxico
+        CrearTexto ct=new CrearTexto();
+        ct.creaArchivoTexto("D:\\", "salida",lineas);
+        
+//        System.out.println(lineas1+"");
+        
+        //Analizador Sintáctico
+         
+        Sintactico sx = new Sintactico(lineas1);
+   
     
-    
+      }
+      
+      public void recuperar(int nrolineas)
+      {
+          lineas1=nrolineas;
+//          System.out.println(lineas1+"ya");
       }
 }
