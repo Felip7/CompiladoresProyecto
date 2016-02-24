342a13e9555de68c29941ceb02af042243432801
Felip7 <esteban.diaz@epn.edu.ec>
2016-02-23 23:46:54 -0500

    Update LecturaLexema.java

--- LecturaLexema.java
+++ LecturaLexema.java
@@ -8,75 +8,68 @@ import static Lex.TokenClass.*;
 
 public class LecturaLexema {
 
-public void leerarchivo(String bufferIn, DataInputStream in, BufferedWriter fw1, BufferedReader bf, int lineas) throws IOException {
-        
-        int NumeroLineas=1;
+public void leerarchivo(String buffer, DataInputStream in, BufferedWriter fwriter, 
+                        BufferedReader breader, int lineas) throws IOException {
+    
+    int NumeroLineas=1;
         try
         {
             //mientras no se llegue al fin de archivo
-            while((bufferIn=in.readLine())!=null && bf.readLine()!=null)
+            while((buffer=in.readLine())!=null && breader.readLine()!=null)
             {
                 int i=0;
                 //elimina espacios en blanco al inicio o al final (pero no a la mitad)
-                String cad=bufferIn.trim();
+                String cad=buffer.trim();
                 
                 //recorremos la línea
                 while(i<cad.length())
                 {
                     char t=cad.charAt(i);//vamos leyendo caracter por caracter
-                    
+
                     if(Character.isDigit(t))
                     {
-                        Numeros(t, cad, fw1, NumeroLineas);
-//                        System.out.println(NumeroLineas);
+                        Numeros(cad, fwriter, NumeroLineas);
                         break;
                     }
-                    
                     else if (Character.isLetterOrDigit(cad.charAt(0))==false ) 
                     {
                        if (cad.length()>=2)
                        {
+//                           System.out.println(cad+""+cad.length());
                            if(Character.isLetter(cad.charAt(1)))
                            {
-                               
-                               Identificador(cad, fw1, NumeroLineas);
-                              
+                               Identificador(cad, fwriter, NumeroLineas);
+                               break;
+                           }
+                           else if(Character.isLetter(cad.charAt(2)))
+                           {
+                               Identificador(cad, fwriter, NumeroLineas);
                                break;
                            }
                            else
                            {
-//                               System.out.println("kk"+NumeroLineas);
-                               Especial(t,cad, fw1, NumeroLineas);
+                               Especial(t,cad, fwriter, NumeroLineas);
                                break;
                            }
                        }
                        else if (Character.isLetter(cad.charAt(0))==false)
                        {
-                          
-//                          System.out.println(NumeroLineas);
-                          Especial(t, cad, fw1, NumeroLineas);
-                          
+                          Especial(t, cad, fwriter, NumeroLineas);
                           break;
                        }
                     }
                     if(Character.isLetter(cad.charAt(0)))
                     {
-//                        System.out.println(NumeroLineas);
-                        Identificador(cad, fw1, NumeroLineas);
-                        
+                        Identificador(cad, fwriter, NumeroLineas);
                         break;
-                    }}
+                    }
+                }
                 i++;  
                 NumeroLineas++;
-                
                 Etapas et=new Etapas();
                 et.recuperar(NumeroLineas);
                 }
-//              NumeroLineas++;
         }catch (IOException ex) {
-            Logger.getLogger(LecturaLexema.class.getName()).log(Level.SEVERE, null, ex);
-        }
-       
+            Logger.getLogger(LecturaLexema.class.getName()).log(Level.SEVERE, null, ex);}
     }
-    
 }
