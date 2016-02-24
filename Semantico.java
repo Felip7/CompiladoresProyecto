6ab4af1b5bbed352842bd96271b31ecc2404665c
Felip7 <esteban.diaz@epn.edu.ec>
2016-02-23 23:52:32 -0500

    Update and rename Semantico to Semantico.java

---
+++ Semantico.java
@@ -0,0 +1,30 @@
+package Semantics;
+
+import java.io.*;
+
+public class Semantico {
+    
+public  BufferedWriter bw1;
+        
+public Semantico(int lineas1) throws FileNotFoundException, IOException{ 
+     
+    File archivo = new File("D:\\tipos.txt");
+        try 
+            {
+                bw1 = new BufferedWriter(new FileWriter(archivo, false));
+            } catch(Exception e) {
+           System.out.println("Error de escritura del fichero");
+           System.out.println(e.getMessage());
+        }
+        extraerSintactico(lineas1);
+}
+
+public void extraerSintactico(int lineas1) throws FileNotFoundException, IOException
+{
+     String bufferIn = "";
+     DataInputStream in=new DataInputStream(new FileInputStream("D:\\simbolos.txt"));
+     FileReader fr1 = new FileReader("D:\\simbolos.txt");
+     BufferedReader br = new BufferedReader(fr1);
+     Extraer e=new Extraer("D:\\simbolos.txt", br, bufferIn, in, lineas1);    
+}
+}
