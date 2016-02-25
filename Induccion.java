2c4f4609874882538eb77477a66a2d6d36789000
Felip7 <esteban.diaz@epn.edu.ec>
2016-02-25 09:15:43 -0500

    Update Induccion.java

--- Induccion.java
+++ Induccion.java
@@ -1,60 +1,137 @@
 package Semantics;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.util.List;
+
+import java.io.*;
+import java.util.*;
 
 public class Induccion {
+    String[] inducc;
     
-     public Induccion(List<List<String>> g, BufferedWriter fw1) throws IOException {
+     public Induccion(List<List<String>> f, List<List<String>> g, 
+        int i, int lineas, String[] p, String[] coincidenciaslineas, BufferedWriter fw1) throws IOException {
+          
+         //Remover nulos
+         String[] cl= new String[coincidenciaslineas.length];
+         System.arraycopy(coincidenciaslineas, 0, cl, 0, coincidenciaslineas.length);
          
-         System.out.println(g.toString()+"");
-         for (int i = 0; i < g.size(); i++) {
-//        System.out.println(i+" "+g.get(2).get(0));
-        if (g.get(i).get(0).equals("string") || g.get(i).get(0).equals("char")) 
-        {
-//            i = i + 1;
-            System.out.println("No existe inducción de tipos para string o char en línea " +i);
-        }
-        else if (g.get(i).get(0).equals("int")||g.get(i).get(0).equals("float")) 
-        {
-            for (int j = 1; j < g.get(i).size() ; j++) {
-                
-                if(!g.get(i).get(j).equals("int")|| !g.get(i).get(j).equals("float")|| !g.get(i).get(j).equals("bool"))
-                { 
-                    i = i + 1;
-                    System.out.println("No existe inducción posible en línea "+i);
-                    fw1.write(i + " N.I.");
-                    fw1.newLine();
-                    fw1.flush();
-                }
-                else
-                {
-                     i=i+1;
-                System.out.println("No existe inducción posible en línea "+i);
-                    fw1.write(i + " N.I.");
-                    fw1.newLine();
-                    fw1.flush();
-                }
+         List<String> list = new ArrayList<>();
+         for(String s : cl) {
+             if(s != null && s.length() > 0) {
+                 list.add(s);
+             }
+         }
+         cl = list.toArray(new String[list.size()]);
+         
+         System.out.println(Arrays.toString(cl)+""); 
+         
+         inducc=new String[g.size()];
+         
+         System.out.println("\nInducción de Tipos");
+         System.out.println("VARIABLES ENCONTRADAS");
+            
+         for ( i = 0; i < g.size(); i++) 
+         {
+            if (g.get(i).get(0).equals("int")||g.get(i).get(0).equals("float")) 
+            {
+                     int e=i+1;
+                     
+                     if(g.get(i).size()>1 && !Character.isLetterOrDigit((g.get(i).get(1).charAt(0))))
+                     {
+                         inducc[i]=g.get(i).get(0)+" "+g.get(i).get(1)+" "+g.get(i).get(2);
+                         
+                         int l = g.size() - 1;
+                         int nrol;
+                         
+                         for (int k = 0; k < l; k++) {
+                             for (int j = 1; j < g.size(); j++) {   
+                                if (g.get(k).size() > 1 && g.get(j).size() > 1 && k != j) {
+                                    if (g.get(k).get(1).equals("=") || g.get(j).get(1).equals(";")
+                                        ||g.get(j).get(2).equals("(")||g.get(j).get(2).equals(")")
+                                        ||g.get(k).get(1).equals("(") || g.get(j).get(1).equals(")")
+                                        ||g.get(j).get(2).equals("=")||g.get(j).get(2).equals(";")){}
+                                    else if (g.get(k).get(1).equals(g.get(j).get(1))) 
+                                    {
+                                        nrol = j + 1;
+                                        System.out.println("\nUso de variable en línea " + nrol);
+//                        System.out.println(g.get(k).get(1)+(g.get(j).get(1)));
+                                        break;
+                                    }
+                                    else if (g.get(k).get(1).equals(g.get(j).get(2)))
+                                    {
+                                        nrol = j + 1;
+                                        System.out.println("Uso de variable "+g.get(k).get(1)+" en línea "
+                                        + j+" y en línea "+nrol+". Sin posible inducción"); 
+                                        break;
+                                    }
+                                } 
+                            } break;
+                        }
+                } 
+//                System.out.println(Arrays.toString(inducc)+""); 
+//                else
+//                {
+//                    inducc[i]=g.get(i).get(0)+" "+g.get(i).get(1);
+//                }
             }
-        }
-        
-        else if(!Character.isLetterOrDigit(g.get(i).get(0).charAt(0)))
-        {
-//             i=i+1;
-            System.out.println("No existe inducción posible (simbolo) en línea "+i);
-                    fw1.write(i + " N.I.");
-                    fw1.newLine();
-                    fw1.flush();
-        }
-        else
-        {
-            i=i+1;
-            System.out.println("No existe inducción posible (letra) en línea "+i);
-                    fw1.write(i + " N.I.");
-                    fw1.newLine();
-                    fw1.flush();
-        }
+            else if (g.get(i).get(0).equals("string") || g.get(i).get(0).equals("char")) 
+            {
+                i = i + 1;
+                System.out.println("No existe inducción de tipos para string o char en línea " +i);
+            }
+         }
+         
+            
+                
+         for (i = 0; i < g.size(); i++) {
+             for (int j = 0; j < p.length; j++) {
+                    if(g.get(i).contains(p[j]))
+                    {
+//                        int e=i+1;
+//                         System.out.println(e+" Coincidencia "+g.get(i).indexOf(p[j])
+//                         +"                "+g.get(i).get(1)+"  "+p[j]);
+                    }
+                    
+             }
+         }
      }
-         System.out.println("\nNo existen más Inducciones o Inferencias por comprobar.");
-     }
-    
+//         
+//     
+//
+//            for (int j = 1; j < g.get(i).size() ; j++) {
+//                
+//                if(!g.get(i).get(j).equals("int")|| !g.get(i).get(j).equals("float")|| !g.get(i).get(j).equals("bool"))
+//                { 
+//                    i = i + 1;
+//                    System.out.println("No existe inducción posible en línea "+i);
+//                    fw1.write(i + " N.I.");
+//                    fw1.newLine();
+//                    fw1.flush();
+//                }
+//                 else if(!Character.isLetterOrDigit(g.get(i).get(0).charAt(0)))
+//        {
+////             i=i+1;
+//            System.out.println("No existe inducción posible (simbolo) en línea "+i);
+//                    fw1.write(i + " N.I.");
+//                    fw1.newLine();
+//                    fw1.flush();
+//        }
+//        else
+//        {
+//            i=i+1;
+//            System.out.println("No existe inducción posible (letra) en línea "+i);
+//                    fw1.write(i + " N.I.");
+//                    fw1.newLine();
+//                    fw1.flush();
+//        }
+//     }
+//         System.out.println("\nNo existen más Inducciones o Inferencias por comprobar.");
+//     }
+////                else
+////                {
+////                     i=i+1;
+////                System.out.println("No existe inducción posible en línea "+i);
+////                    fw1.write(i + " N.I.");
+////                    fw1.newLine();
+////                    fw1.flush();
+////                }
+
+     
