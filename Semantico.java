da0d17a6525e82cbb5ee8cd98aa2c9a959d6072a
Felip7 <esteban.diaz@epn.edu.ec>
2016-02-25 09:14:35 -0500

    Update Semantico.java

--- Semantico.java
+++ Semantico.java
@@ -16,15 +16,15 @@ public Semantico(int lineas1) throws FileNotFoundException, IOException{
            System.out.println("Error de escritura del fichero");
            System.out.println(e.getMessage());
         }
-        extraerSintactico(lineas1);
+        extraerSintactico(lineas1,bw1);
 }
 
-public void extraerSintactico(int lineas1) throws FileNotFoundException, IOException
+public void extraerSintactico(int lineas1, BufferedWriter bw1) throws FileNotFoundException, IOException
 {
      String bufferIn = "";
      DataInputStream in=new DataInputStream(new FileInputStream("D:\\simbolos.txt"));
      FileReader fr1 = new FileReader("D:\\simbolos.txt");
      BufferedReader br = new BufferedReader(fr1);
-     Extraer e=new Extraer("D:\\simbolos.txt", br, bufferIn, in, lineas1);    
+     Extraer e=new Extraer("D:\\simbolos.txt", bw1, br, bufferIn, in, lineas1);    
 }
 }
