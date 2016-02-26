bfc4efc2cd8092462faa7defe87897a48bfc4135
Felip7 <esteban.diaz@epn.edu.ec>
2016-02-26 00:59:35 -0500

    Update Etapas.java

--- Etapas.java
+++ Etapas.java
@@ -7,33 +7,26 @@ import Syntax.Sintactico;
 import java.io.IOException;
 
 public class Etapas {
-    
     public static int lineas;
     public static int lineas1;
     
-      public static void main(String args[]) throws IOException {
-//          Analizador Léxico
+      public static void main(String args[]) throws IOException 
+      {
+
+        System.out.println("LÉXICO");
         CrearTexto ct=new CrearTexto();
         ct.creaArchivoTexto("D:\\", "salida",lineas);
-        
-//        System.out.println(lineas1+"");
-        
-        //Analizador Sintáctico
+
+        System.out.println("\nSINTACTICO");
         Sintactico sx = new Sintactico(lineas1);
-   
-        //Analizador Semántico
+        
+        System.out.println("\nSEMANTICO");
         Semantico scs=new Semantico(lineas1);
         
-        //Generación de Código
         Gencod gcd=new Gencod(lineas1);
-    
+
       }
       
       public void recuperar(int nrolineas)
-      {
-          lineas1=nrolineas;
-//          System.out.println(lineas1+"ya");
-      }
-}
-
+      { lineas1=nrolineas;  }
 }
